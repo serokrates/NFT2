53ecd538cc1f3cc52b6a345a7f1a5f97e9191695
serokrates <homm5@vp.pl>
2021-12-14 12:09:59 +0100

    Updates

---
+++ precache-manifest.e29f5061742c6ecb5251e08f9b8768b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbae0898545f499b499e",
+    "url": "/NFT2/static/css/main.a48c126a.chunk.css"
+  },
+  {
+    "revision": "cbae0898545f499b499e",
+    "url": "/NFT2/static/js/main.cbae0898.chunk.js"
+  },
+  {
+    "revision": "6afbf72fb1369b319f64",
+    "url": "/NFT2/static/css/1.5fc36d8f.chunk.css"
+  },
+  {
+    "revision": "6afbf72fb1369b319f64",
+    "url": "/NFT2/static/js/1.6afbf72f.chunk.js"
+  },
+  {
+    "revision": "ce8171bca1d6586c9557",
+    "url": "/NFT2/static/js/runtime~main.ce8171bc.js"
+  },
+  {
+    "revision": "bdf2a4a1506ecbad79e64ea4f770609e",
+    "url": "/NFT2/static/media/3.bdf2a4a1.png"
+  },
+  {
+    "revision": "7db777d229df5665fa3455268297f645",
+    "url": "/NFT2/static/media/2.7db777d2.png"
+  },
+  {
+    "revision": "e41074765930e6b7de4fbb9834274e67",
+    "url": "/NFT2/index.html"
+  }
+];
